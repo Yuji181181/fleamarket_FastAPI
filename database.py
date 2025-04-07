4242b48f85e2155009e668a76ab65c6608cf303a
Yuji181181 <s1f102402552@iniad.org>
2025-04-08 03:53:30 +0900

    修正

--- database.py
+++ database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-SQLITE_DATABASE_URL = "postgresql://test1:fastapipass@127.0.0.1:5432/test1"
+SQLALCHEMY_DATABASE_URL = "postgresql://test1:fastapipass@127.0.0.1:5432/test1"
 
-engine = create_engine(SQLITE_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
